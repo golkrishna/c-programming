62cdde284e9131240dbd3dcd80c4ea28873f6d26
golkrishna <121308011+golkrishna@users.noreply.github.com>
2023-05-09 10:49:31 +0530

    Create ar(rect).c

---
+++ ar(rect).c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	int l,b;
+	printf("Enter length:");
+	scanf("%d",&l);
+	printf("Enter breathe:");
+	scanf("%d",&b);
+	printf("Area is %d",(l*b));
+	return 0;
+}
