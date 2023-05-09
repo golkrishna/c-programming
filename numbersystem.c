d732eca46cc56adda897d15edd0fac03d4937702
golkrishna <121308011+golkrishna@users.noreply.github.com>
2023-05-09 11:09:04 +0530

    Create numbersystem.c

---
+++ numbersystem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int value;
+    scanf("%d",&value);
+    printf("%i\n",value);
+    printf("%o\n",value);
+    printf("%x\n",value);
+    printf("%X",value);
+   
+    return 0;
+}
